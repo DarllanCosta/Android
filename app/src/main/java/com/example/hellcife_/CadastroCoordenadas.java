9b005cbbf0a8b321557b2a13f8a012d3b135c115
Darllan Costa <darllancosta7@gmail.com>
2019-10-25 05:24:52 -0300

    Cadastro no firebase pegando

--- app/src/main/java/com/example/hellcife_/CadastroCoordenadas.java
+++ app/src/main/java/com/example/hellcife_/CadastroCoordenadas.java
@@ -3,18 +3,30 @@ package com.example.hellcife_;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.hellcife_.ui.Denuncia;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 
 public class CadastroCoordenadas extends AppCompatActivity {
+
+    EditText editLatitude;
+    EditText editLongitude;
+    EditText editDescricao;
+    Spinner editTipo;
+    FirebaseDatabase fbDB;
+    DatabaseReference bdDenuncia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,10 @@ public class CadastroCoordenadas extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro_coordenadas);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fbDB = FirebaseDatabase.getInstance();
+        bdDenuncia = fbDB.getReference("Denuncia");
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,17 +48,32 @@ public class CadastroCoordenadas extends AppCompatActivity {
             }
         });
 
-
+        editLatitude = (EditText) findViewById(R.id.latitude);
+        editLongitude = (EditText) findViewById(R.id.longitude);
+        editDescricao = (EditText) findViewById(R.id.descricao);
+        editTipo = (Spinner) findViewById(R.id.tipoDenuncia);
         Intent intent = getIntent();
         Bundle bundle = intent.getParcelableExtra("cd");
         LatLng coordenadas = bundle.getParcelable("coordenadas");
-        String message = coordenadas.toString();
-        EditText editText = (EditText) findViewById(R.id.coordenadas);
-        editText.setText(message);
+        double latitude = coordenadas.latitude;
+        double longitude = coordenadas.longitude;
+        editLongitude.setText(Double.toString(longitude));
+        editLatitude.setText(Double.toString(latitude));
+
 
 
     }
 
+
+
+    public void clickDenuncia(View view){
+        final String latitude = editLatitude.getText().toString();
+        final String longitude = editLongitude.getText().toString();
+        final String descricao = editDescricao.getText().toString();
+        final String tipo = editTipo.getSelectedItem().toString();
+        Denuncia denuncia = new Denuncia(latitude, longitude, descricao, tipo);
+        bdDenuncia.push().setValue(denuncia);
+    }
 
 
 
--- app/src/main/java/com/example/hellcife_/ui/Denuncia.java
+++ app/src/main/java/com/example/hellcife_/ui/Denuncia.java
@@ -3,49 +3,38 @@ package com.example.hellcife_.ui;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.IgnoreExtraProperties;
 
-import java.util.Date;
+
 
 @IgnoreExtraProperties
 public class Denuncia {
     String descricao;
     String tipo;
-    Date data;
-    LatLng localizacao;
+    String latitude;
+    String longitude;
 
     public Denuncia(){
 
     }
 
-    public Denuncia(String descricao, String tipo, Date data, LatLng localizacao){
+    public Denuncia(String latitude, String longitude, String descricao, String tipo){
         this.descricao = descricao;
         this.tipo = tipo;
-        this.data = data;
-        this.localizacao = localizacao;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
-    public void setData(Date data) {
-        this.data = data;
-    }
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
-    public void setLocalizacao(LatLng localizacao) {
-        this.localizacao = localizacao;
-    }
+
 
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
 
-    public Date getData() {
-        return data;
-    }
-
-    public LatLng getLocalizacao() {
-        return localizacao;
-    }
+    public String getLongitude() {return longitude; }
 
     public String getDescricao() {
         return descricao;
@@ -53,5 +42,17 @@ public class Denuncia {
 
     public String getTipo() {
         return tipo;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
     }
 }
